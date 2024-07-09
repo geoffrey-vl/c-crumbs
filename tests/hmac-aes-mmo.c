4f03d0266e4fbfb834e9d878493a2a9b4bac480f
Geoffrey Van Landeghem <geoffrey.vanlandeghem@niko.eu>
2024-07-09 19:33:21 +0200

    add another hmac-aes-mmo test

--- tests/hmac-aes-mmo.c
+++ tests/hmac-aes-mmo.c
@@ -47,7 +47,8 @@ int main(int argc, char **argv) {
             return 1;
         }
     }
-    /* [ZigbeeDirect] mimic 'sl_zb_sec_man_hmac_aes_mmo' */
+
+    /* [ZigbeeDirect] mimic 'sl_zb_sec_man_hmac_aes_mmo' Test 1*/
     {
         const unsigned char plaintext[] = {
             0x4B,0x43,0x5F,0x32,0x5F,0x55,0xFE,0xD5,0x4B,0xFE,0xFF,0xE2,0x0A,0x68,0x8A,0xAB,
@@ -59,6 +60,70 @@ int main(int argc, char **argv) {
         const unsigned char key[16] = {0x4A,0xD3,0x1E,0xB7,0x97,0x7D,0x55,0xD1,0x10,0x70,0x50,0x41,0x95,0xB7,0xFA,0x08};
         const unsigned char ciphertext_expected[16] = {0x41,0x15,0x62,0xA5,0xBE,0x3E,0xDB,0xCE,0x03,0x4C,0xBD,0x7C,0x71,0x52,0x90,0xC7};
         unsigned char ciphertext_calculated[16];
+
+        hmac_aes_mmo(ciphertext_calculated, plaintext, sizeof(plaintext), key);
+        print_result("[ZigbeeDirect] mimic 'sl_zb_sec_man_hmac_aes_mmo'", plaintext, sizeof(plaintext), key, ciphertext_calculated, ciphertext_expected);
+        if (memcmp(ciphertext_expected, ciphertext_calculated, 16)) {
+            fputs("aes_mmo() failed test vector\n", stderr);
+            return 1;
+        }
+    }
+
+    /* [ZigbeeDirect] mimic 'sl_zb_sec_man_hmac_aes_mmo' Test 2*/
+    {
+        const unsigned char di_reverse[8] = {
+          0x01, 0x00, 0x00, 0x00, 0x00, 0xee, 0x1f, 0x00
+        };
+        const unsigned char dr_reverse[8] = {
+          0x02, 0x00, 0x00, 0x00, 0x00, 0xee, 0x1f, 0x00
+        };
+        const unsigned char qi[32] = {
+          0x5d, 0x28, 0x29, 0x8c, 0x91, 0x3d, 0x58, 0x8a,
+          0x35, 0xe0, 0x1f, 0x79, 0xc9, 0x11, 0x66, 0x10,
+          0xb7, 0x9b, 0x38, 0xe9, 0xc6, 0xcf, 0x15, 0x5c,
+          0x55, 0xb0, 0x52, 0x9b, 0xbc, 0xcc, 0x85, 0x56
+        };
+        const unsigned char qr[32] = {
+          0x82, 0x77, 0x2f, 0x75, 0x3d, 0xe9, 0x4e, 0xb4,
+          0x92, 0x0f, 0xb4, 0x17, 0xd4, 0x67, 0x17, 0x80,
+          0x81, 0x74, 0x0c, 0x1a, 0x87, 0x58, 0xfd, 0x3f,
+          0x2f, 0x63, 0x02, 0xb5, 0x0b, 0x98, 0x35, 0x6f
+        };
+        unsigned char plaintext[ 6 +
+                                 sizeof(di_reverse) +
+                                 sizeof(dr_reverse) +
+                                 sizeof(qi) + 
+                                 sizeof(qr)
+                          ] = { 0 };
+        const unsigned char key[16] = {0xaf,0xf1,0x93,0x4b,0xe6,0x9b,0x40,0x11,0x17,0x9d,0x81,0xd9,0xf9,0xa5,0xff,0xeb};
+        const unsigned char ciphertext_expected[16] = {0xda,0x21,0xb1,0x04,0xd0,0x73,0x96,0x6f,0x5a,0x38,0x80,0x27,0x2f,0x8a,0xf2,0xf0};
+        unsigned char ciphertext_calculated[16];
+
+        // 'KC_2_U'
+        plaintext[0] = 0x4B;
+        plaintext[1] = 0x43;
+        plaintext[2] = 0x5F;
+        plaintext[3] = 0x32;
+        plaintext[4] = 0x5F;
+        plaintext[5] = 0x55;
+        // m = xk || di || dr || Qi || Qr || G
+        int offset = 6;
+        int i;
+        for (i=0; i<sizeof(dr_reverse); i++) {
+          plaintext[offset+i] = dr_reverse[i];
+        }
+        offset += sizeof(dr_reverse);
+        for (i=0; i<sizeof(qr); i++) {
+          plaintext[offset+i] = qr[i];
+        }
+        offset += sizeof(qr);
+        for (i=0; i<sizeof(di_reverse); i++) {
+          plaintext[offset+i] = di_reverse[i];
+        }
+        offset += sizeof(di_reverse);
+        for (i=0; i<sizeof(qi); i++) {
+          plaintext[offset+i] = qi[i];
+        }
 
         hmac_aes_mmo(ciphertext_calculated, plaintext, sizeof(plaintext), key);
         print_result("[ZigbeeDirect] mimic 'sl_zb_sec_man_hmac_aes_mmo'", plaintext, sizeof(plaintext), key, ciphertext_calculated, ciphertext_expected);

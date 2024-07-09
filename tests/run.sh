cbd52904b61d27f15a8b45caa465e1dde26008da
Geoffrey Van Landeghem <geoffrey.vanlandeghem@niko.eu>
2024-07-09 16:40:26 +0200

    c compile only

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 for c in $*; do
-	gcc -Wall -Werror -ansi -pedantic -O2 $c
+	gcc -Wall -Werror -ansi -pedantic -std=c11 -O2 $c
 	./a.out
-	g++ -Wall -Werror -O2 $c
-	./a.out
+	#g++ -Wall -Werror -O2 $c
+	#./a.out
 done
